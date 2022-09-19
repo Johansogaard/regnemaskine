0ebce46010a1340fac3df1b1740e293141d784e8
Johan Jorgensen <Johansogaardjorgensen@gmail.com>
2022-09-19 15:17:34 +0200

    regnemaksine

--- src/repo.java
+++ src/repo.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class repo {
+import java.util.Scanner;
+
+public class repo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a+ "+"+b+"="+(a+b));
+        System.out.println(a+ "-"+b+"="+(a-b));
+
+        //TESTTESTETSTEST
+
+        System.out.println("HEJEHEJEHEJEHEJE");
+    }
 }
